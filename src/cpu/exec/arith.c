72dc46f9d7462feffe1507f0dbecdc8f1c26bcdc
Zihao Yu <19884013+sashimi-yzh@users.noreply.github.com>
2017-11-07 18:19:20 +0800

    cpu,exec,arith: add spaces

--- src/cpu/exec/arith.c
+++ src/cpu/exec/arith.c
@@ -46,7 +46,7 @@ make_EHelper(adc) {
   rtl_update_ZFSF(&t2, id_dest->width);
 
   rtl_sltu(&t0, &t2, &id_dest->val);
-  rtl_or(&t0,&t3,&t0);
+  rtl_or(&t0, &t3, &t0);
   rtl_set_CF(&t0);
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
@@ -69,7 +69,7 @@ make_EHelper(sbb) {
   rtl_update_ZFSF(&t2, id_dest->width);
 
   rtl_sltu(&t0, &id_dest->val, &t2);
-  rtl_or(&t0,&t3,&t0);
+  rtl_or(&t0, &t3, &t0);
   rtl_set_CF(&t0);
 
   rtl_xor(&t0, &id_dest->val, &id_src->val);
