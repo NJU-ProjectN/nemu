c880b79490b33824995bfa4eb78b5f258ea735c5
Zihao Yu <yuzihao@ict.ac.cn>
2020-09-12 18:36:39 +0800

    memory,paddr: match memory size to gitbook

--- include/memory/paddr.h
+++ include/memory/paddr.h
@@ -3,7 +3,7 @@
 
 #include <common.h>
 
-#define PMEM_SIZE (256 * 1024 * 1024)
+#define PMEM_SIZE (128 * 1024 * 1024)
 
 /* convert the guest physical address in the guest program to host virtual address in NEMU */
 void* guest_to_host(paddr_t addr);
