b4cf42b47bf2b9ecb75e3ec5f9d850a1fef67057
Yujia Qiao <contact@rapiz.me>
2021-02-16 18:30:40 +0800

    Make BITMASK macro safer
    Use 1ull in shifting to avoid undefined behavior
    
    If users call the original BITMASK(32), the result is undefined.
    See also https://stackoverflow.com/questions/2648764/whats-bad-about-shifting-a-32-bit-variable-32-bits

--- include/macro.h
+++ include/macro.h
@@ -12,7 +12,7 @@
 
 #define MAP(c, f) c(f)
 
-#define BITMASK(bits) ((1 << (bits)) - 1)
+#define BITMASK(bits) ((1ull << (bits)) - 1)
 #define BITS(x, hi, lo) (((x) >> (lo)) & BITMASK((hi) - (lo) + 1)) // similar to x[hi:lo] in verilog
 #define SEXT(x, len) ({ struct { int64_t n : len; } __x = { .n = x }; (int64_t)__x.n; })
 
