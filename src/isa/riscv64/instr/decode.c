b9af1ea9abd93b169254ec6c4d3e6af55780a513
Zihao Yu <yuzihao@ict.ac.cn>
2021-08-19 17:50:05 +0800

    *,instr,decode: rename `load_val` to `is_write`

--- src/isa/riscv64/instr/decode.c
+++ src/isa/riscv64/instr/decode.c
@@ -18,28 +18,28 @@ static inline def_DopHelper(i) {
 }
 
 static inline def_DopHelper(r) {
-  bool load_val = flag;
+  bool is_write = flag;
   static word_t zero_null = 0;
-  op->preg = (!load_val && val == 0) ? &zero_null : &gpr(val);
+  op->preg = (is_write && val == 0) ? &zero_null : &gpr(val);
   print_Dop(op->str, OP_STR_SIZE, "%s", reg_name(val, 4));
 }
 
 static inline def_DHelper(I) {
-  decode_op_r(s, id_src1, s->isa.instr.i.rs1, true);
+  decode_op_r(s, id_src1, s->isa.instr.i.rs1, false);
   decode_op_i(s, id_src2, (sword_t)s->isa.instr.i.simm11_0, false);
-  decode_op_r(s, id_dest, s->isa.instr.i.rd, false);
+  decode_op_r(s, id_dest, s->isa.instr.i.rd, true);
 }
 
 static inline def_DHelper(U) {
   decode_op_i(s, id_src1, (sword_t)s->isa.instr.u.simm31_12 << 12, true);
-  decode_op_r(s, id_dest, s->isa.instr.u.rd, false);
+  decode_op_r(s, id_dest, s->isa.instr.u.rd, true);
 }
 
 static inline def_DHelper(S) {
-  decode_op_r(s, id_src1, s->isa.instr.s.rs1, true);
+  decode_op_r(s, id_src1, s->isa.instr.s.rs1, false);
   sword_t simm = (s->isa.instr.s.simm11_5 << 5) | s->isa.instr.s.imm4_0;
   decode_op_i(s, id_src2, simm, false);
-  decode_op_r(s, id_dest, s->isa.instr.s.rs2, true);
+  decode_op_r(s, id_dest, s->isa.instr.s.rs2, false);
 }
 
 def_THelper(load) {
