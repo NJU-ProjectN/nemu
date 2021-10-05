c02f89e0a88202ef497bbf048c105790c67626c8
Zihao Yu <yuzihao@ict.ac.cn>
2021-10-05 09:03:42 +0800

    monitor,sdb,watchpoint: fix array out-of-bound

--- src/monitor/sdb/watchpoint.c
+++ src/monitor/sdb/watchpoint.c
@@ -17,9 +17,8 @@ void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
-    wp_pool[i].next = &wp_pool[i + 1];
+    wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
   }
-  wp_pool[NR_WP - 1].next = NULL;
 
   head = NULL;
   free_ = wp_pool;
