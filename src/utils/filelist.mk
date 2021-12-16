3ee391d5999dde5de773b38fafcc76d534ba86ba
OkitaSan <tssjy999@163.com>
2021-12-16 13:31:06 +0800

    bug fixed

--- src/utils/filelist.mk
+++ src/utils/filelist.mk
@@ -1,5 +1,13 @@
 ifdef CONFIG_ITRACE
 CXXSRC = src/utils/disasm.cc
-CXXFLAGS += $(shell llvm-config-11 --cxxflags) -fPIE
-LIBS += $(shell llvm-config-11 --libs)
+LLVM_VERSION=$(shell  llvm-config --version 2>/dev/null || echo "Not found")
+ifeq ($(LLVM_VERSION),13.0.0)
+        LLVM_CONFIG = llvm-config
+else
+        LLVM_CONFIG = llvm-config-11
+endif
+T1 = $(echo LLVM_VERSION)
+T2 = $(echo LLVM_CONFIG)
+CXXFLAGS += $(shell $(LLVM_CONFIG) --cxxflags) -fPIE
+LIBS += $(shell $(LLVM_CONFIG) --libs)
 endif
