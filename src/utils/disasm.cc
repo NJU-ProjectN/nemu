d21e335ca8903670db422a8717956fa5c7fa92a3
ruxiliang <91934643+ruxiliang@users.noreply.github.com>
2021-12-17 13:33:38 +0800

    Fix compile error with llvm 13
    The API `llvm::MCContext` changes in llvm 13. We use conditional compilation to support it.

--- src/utils/disasm.cc
+++ src/utils/disasm.cc
@@ -41,7 +41,13 @@ extern "C" void init_disasm(const char *triple) {
   gMII = target->createMCInstrInfo();
   gMRI = target->createMCRegInfo(gTriple);
   auto AsmInfo = target->createMCAsmInfo(*gMRI, gTriple, MCOptions);
-  auto Ctx = new llvm::MCContext(AsmInfo, gMRI, nullptr);
+#if LLVM_VERSION_MAJOR == 13
+   auto llvmTripleTwine = Twine(triple);
+   auto llvmtriple = llvm::Triple(llvmTripleTwine);
+   auto Ctx = new llvm::MCContext(llvmtriple,AsmInfo, gMRI, nullptr);
+#elif LLVM_VERSION_MAJOR == 11
+   auto Ctx = new llvm::MCContext(AsmInfo, gMRI, nullptr);
+#endif
   gDisassembler = target->createMCDisassembler(*gSTI, *Ctx);
   gIP = target->createMCInstPrinter(llvm::Triple(gTriple),
       AsmInfo->getAssemblerDialect(), *AsmInfo, *gMII, *gMRI);
