d2de05d7d3f2ef8ae13403bc687be4a1a19c4aac
Zihao Yu <yuzihao@ict.ac.cn>
2022-03-15 22:39:19 +0800

    cpu,difftest,dut: remove unnecessary RTLD_DEEPBIND in dlopen()

--- src/cpu/difftest/dut.c
+++ src/cpu/difftest/dut.c
@@ -48,7 +48,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   assert(ref_so_file != NULL);
 
   void *handle;
-  handle = dlopen(ref_so_file, RTLD_LAZY | MUXNDEF(CONFIG_CC_ASAN, RTLD_DEEPBIND, 0));
+  handle = dlopen(ref_so_file, RTLD_LAZY);
   assert(handle);
 
   ref_difftest_memcpy = dlsym(handle, "difftest_memcpy");
