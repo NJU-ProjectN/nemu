a0a70ead187a928ad4e55cf3a5c72ef8a3734475
Zihao Yu <yuzihao@ict.ac.cn>
2022-04-09 12:31:54 +0800

    utils,disasm: TargetRegistry.h is moved since llvm-14

--- src/utils/disasm.cc
+++ src/utils/disasm.cc
@@ -7,7 +7,11 @@
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCDisassembler/MCDisassembler.h"
 #include "llvm/MC/MCInstPrinter.h"
+#if LLVM_VERSION_MAJOR >= 14
+#include "llvm/MC/TargetRegistry.h"
+#else
 #include "llvm/Support/TargetRegistry.h"
+#endif
 #include "llvm/Support/TargetSelect.h"
 
 #if defined(__GNUC__) && !defined(__clang__)
