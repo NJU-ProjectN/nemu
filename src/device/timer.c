d3416b4a63b497f1b2e9a7052fb6d3327b59ab29
FpguDhk <35809384+341101@users.noreply.github.com>
2022-06-14 15:49:46 +0000

    [Fix] 修复读取时间时rtc_io_handler未能及时更新时间的问题

--- src/device/timer.c
+++ src/device/timer.c
@@ -6,7 +6,7 @@ static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
-  if (!is_write && offset == 4) {
+  if (!is_write && offset == 0) {
     uint64_t us = get_time();
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
