bf47a5911819fc1010f5d649f784113afa06b026
lu chen <181250012@smail.nju.edu.cn>
2022-08-21 19:49:52 +0800

    fix format for uint64

--- src/cpu/cpu-exec.c
+++ src/cpu/cpu-exec.c
@@ -80,7 +80,7 @@ static void execute(uint64_t n) {
 
 static void statistic() {
   IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
-#define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%ld", "%'ld")
+#define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%lld", "%'lld")
   Log("host time spent = " NUMBERIC_FMT " us", g_timer);
   Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
   if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
