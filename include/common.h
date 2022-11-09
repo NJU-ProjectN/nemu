947dc940a9c9efcbdc798354eb71d85803bc4220
Zihao Yu <yuzihao@ict.ac.cn>
2022-11-09 15:51:25 +0800

    common.h: fix "invalid suffix on literal" warning for g++
    * "C++11 requires a space between literal and string macro"

--- include/common.h
+++ include/common.h
@@ -37,11 +37,11 @@
 
 typedef MUXDEF(CONFIG_ISA64, uint64_t, uint32_t) word_t;
 typedef MUXDEF(CONFIG_ISA64, int64_t, int32_t)  sword_t;
-#define FMT_WORD MUXDEF(CONFIG_ISA64, "0x%016"PRIx64, "0x%08"PRIx32)
+#define FMT_WORD MUXDEF(CONFIG_ISA64, "0x%016" PRIx64, "0x%08" PRIx32)
 
 typedef word_t vaddr_t;
 typedef MUXDEF(PMEM64, uint64_t, uint32_t) paddr_t;
-#define FMT_PADDR MUXDEF(PMEM64, "0x%016"PRIx64, "0x%08"PRIx32)
+#define FMT_PADDR MUXDEF(PMEM64, "0x%016" PRIx64, "0x%08" PRIx32)
 typedef uint16_t ioaddr_t;
 
 #include <debug.h>
