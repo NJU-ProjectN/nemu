b5c841e21b2e87daedb64dd2710c0e8c38ee5aaf
Zihao Yu <yuzihao@ict.ac.cn>
2023-08-17 13:08:22 +0800

    device,keyboard: fix conflict macro definitions with standard libraries

--- src/device/keyboard.c
+++ src/device/keyboard.c
@@ -22,7 +22,7 @@
 #include <SDL2/SDL.h>
 
 // Note that this is not the standard
-#define _KEYS(f) \
+#define NEMU_KEYS(f) \
   f(ESCAPE) f(F1) f(F2) f(F3) f(F4) f(F5) f(F6) f(F7) f(F8) f(F9) f(F10) f(F11) f(F12) \
 f(GRAVE) f(1) f(2) f(3) f(4) f(5) f(6) f(7) f(8) f(9) f(0) f(MINUS) f(EQUALS) f(BACKSPACE) \
 f(TAB) f(Q) f(W) f(E) f(R) f(T) f(Y) f(U) f(I) f(O) f(P) f(LEFTBRACKET) f(RIGHTBRACKET) f(BACKSLASH) \
@@ -31,23 +31,18 @@ f(LSHIFT) f(Z) f(X) f(C) f(V) f(B) f(N) f(M) f(COMMA) f(PERIOD) f(SLASH) f(RSHIF
 f(LCTRL) f(APPLICATION) f(LALT) f(SPACE) f(RALT) f(RCTRL) \
 f(UP) f(DOWN) f(LEFT) f(RIGHT) f(INSERT) f(DELETE) f(HOME) f(END) f(PAGEUP) f(PAGEDOWN)
 
-#define _KEY_NAME(k) _KEY_##k,
-
-// It's conflicted on macos with sys/_types/_key_t.h
-#ifdef __APPLE__
-  #undef _KEY_T 
-#endif
+#define NEMU_KEY_NAME(k) NEMU_KEY_ ## k,
 
 enum {
-  _KEY_NONE = 0,
-  MAP(_KEYS, _KEY_NAME)
+  NEMU_KEY_NONE = 0,
+  MAP(NEMU_KEYS, NEMU_KEY_NAME)
 };
 
-#define SDL_KEYMAP(k) keymap[concat(SDL_SCANCODE_, k)] = concat(_KEY_, k);
+#define SDL_KEYMAP(k) keymap[concat(SDL_SCANCODE_, k)] = concat(NEMU_KEY_, k);
 static uint32_t keymap[256] = {};
 
 static void init_keymap() {
-  MAP(_KEYS, SDL_KEYMAP)
+  MAP(NEMU_KEYS, SDL_KEYMAP)
 }
 
 #define KEY_QUEUE_LEN 1024
@@ -61,7 +56,7 @@ static void key_enqueue(uint32_t am_scancode) {
 }
 
 static uint32_t key_dequeue() {
-  uint32_t key = _KEY_NONE;
+  uint32_t key = NEMU_KEY_NONE;
   if (key_f != key_r) {
     key = key_queue[key_f];
     key_f = (key_f + 1) % KEY_QUEUE_LEN;
@@ -70,13 +65,13 @@ static uint32_t key_dequeue() {
 }
 
 void send_key(uint8_t scancode, bool is_keydown) {
-  if (nemu_state.state == NEMU_RUNNING && keymap[scancode] != _KEY_NONE) {
+  if (nemu_state.state == NEMU_RUNNING && keymap[scancode] != NEMU_KEY_NONE) {
     uint32_t am_scancode = keymap[scancode] | (is_keydown ? KEYDOWN_MASK : 0);
     key_enqueue(am_scancode);
   }
 }
 #else // !CONFIG_TARGET_AM
-#define _KEY_NONE 0
+#define NEMU_KEY_NONE 0
 
 static uint32_t key_dequeue() {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
@@ -95,7 +90,7 @@ static void i8042_data_io_handler(uint32_t offset, int len, bool is_write) {
 
 void init_i8042() {
   i8042_data_port_base = (uint32_t *)new_space(4);
-  i8042_data_port_base[0] = _KEY_NONE;
+  i8042_data_port_base[0] = NEMU_KEY_NONE;
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("keyboard", CONFIG_I8042_DATA_PORT, i8042_data_port_base, 4, i8042_data_io_handler);
 #else
