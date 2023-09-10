4fbe3ab3ba70a2cc6af0e2698a4041e89ac4dfc0
Zihao Yu <yuzihao@ict.ac.cn>
2023-09-10 23:42:13 +0800

    memory,paddr: randomize pmem with only one random byte
    * this will improve the performance of randomize memory by about 100X

--- src/memory/paddr.c
+++ src/memory/paddr.c
@@ -46,13 +46,7 @@ void init_mem() {
   pmem = malloc(CONFIG_MSIZE);
   assert(pmem);
 #endif
-#ifdef CONFIG_MEM_RANDOM
-  uint32_t *p = (uint32_t *)pmem;
-  int i;
-  for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i ++) {
-    p[i] = rand();
-  }
-#endif
+  IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE));
   Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
 }
 
