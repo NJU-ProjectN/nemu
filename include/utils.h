fc81a8f7b4e9da6c3dff68943082e8094f41bc3c
珞林 <59970710+wele0612@users.noreply.github.com>
2024-03-05 18:11:12 -0800

    Fixed: segmentation fault when calling Log macro too early

--- include/utils.h
+++ include/utils.h
@@ -60,7 +60,7 @@ uint64_t get_time();
   do { \
     extern FILE* log_fp; \
     extern bool log_enable(); \
-    if (log_enable()) { \
+    if (log_enable() && log_fp != NULL) { \
       fprintf(log_fp, __VA_ARGS__); \
       fflush(log_fp); \
     } \
