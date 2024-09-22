27e934a8c52c4e9ffd0de07aac6cffafa190e49e
Zihao Yu <yuzihao@ict.ac.cn>
2024-09-22 17:30:40 +0800

    cpu,decode: remove extra `*` for label address

--- include/cpu/decode.h
+++ include/cpu/decode.h
@@ -96,7 +96,7 @@ finish:
   } \
 } while (0)
 
-#define INSTPAT_START(name) { const void ** __instpat_end = &&concat(__instpat_end_, name);
+#define INSTPAT_START(name) { const void * __instpat_end = &&concat(__instpat_end_, name);
 #define INSTPAT_END(name)   concat(__instpat_end_, name): ; }
 
 #endif
