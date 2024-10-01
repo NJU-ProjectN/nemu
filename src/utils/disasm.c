e752b0f87b1beb956613ecf82a76b932d5dd246b
Shyliuli <139528661+Shyliuli@users.noreply.github.com>
2024-10-01 15:39:02 +0800

    Fix: Update hardcoded libcapstone.so.5 to libcapstone.so.6

--- src/utils/disasm.c
+++ src/utils/disasm.c
@@ -25,7 +25,7 @@ static csh handle;
 
 void init_disasm() {
   void *dl_handle;
-  dl_handle = dlopen("tools/capstone/repo/libcapstone.so.5", RTLD_LAZY);
+  dl_handle = dlopen("tools/capstone/repo/libcapstone.so.6", RTLD_LAZY);
   assert(dl_handle);
 
   cs_err (*cs_open_dl)(cs_arch arch, cs_mode mode, csh *handle) = NULL;
