d0d99e43827dd0c8a9800357cbedcffe7ee31a57
Kedit2007 <251220007@smail.nju.edu.cn>
2026-08-12 01:52:39 +0800

    fix #139 remove -pie in filelist.mk

--- src/filelist.mk
+++ src/filelist.mk
@@ -19,7 +19,7 @@ DIRS-$(CONFIG_MODE_SYSTEM) += src/memory
 DIRS-BLACKLIST-$(CONFIG_TARGET_AM) += src/monitor/sdb
 
 SHARE = $(if $(CONFIG_TARGET_SHARE),1,0)
-LIBS += $(if $(CONFIG_TARGET_NATIVE_ELF),-lreadline -ldl -pie,)
+LIBS += $(if $(CONFIG_TARGET_NATIVE_ELF),-lreadline -ldl,)
 
 ifdef mainargs
 ASFLAGS += -DBIN_PATH=\"$(mainargs)\"
